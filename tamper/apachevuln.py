6818c654210f68b911dbdb02390986aa046dca0c
BTtea <32271123+BTtea@users.noreply.github.com>
2024-11-12 11:33:57 +0800

    Delete tamper/apachevuln.py

--- tamper/apachevuln.py
+++
@@ -1,6 +0,0 @@
-# page=../../../../etc/passwd  =>  page=%2e./%2e./%2e./%2e./etc/passwd
-
-def tamper(payload:str):
-    if payload:
-        payload=payload.replace('../','%2e./')
-    return payload
